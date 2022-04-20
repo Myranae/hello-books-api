d32a8d6bd004416478d7895aa9b24150a9336679
Myranae <myranae@pm.me>
2022-04-20 17:45:12 -0400

    completes problem set: setup and read

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,33 @@
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Red Fish, Blue Fish", "A Dr. Seuss book"),
+    Book(2, "The Grinch", "Another Dr. Seuss book"),
+    Book(3, "The Lorax", "The best Dr. Seuss book"),
+]
 
 hello_world_bp = Blueprint("hello_world_bp", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+@books_bp.route("", methods=["GET"])
+def all_books():
+    books_json = []
+    for book in books:
+        books_json.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
+        )
+    return jsonify(books_json), 200 
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
