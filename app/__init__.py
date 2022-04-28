d9f45410b91803e10e996586b5dd5644f4a6001c
Myranae <myranae@pm.me>
2022-04-28 12:02:46 -0400

    sets up a db and migrate; connects up to db and migrate; runs migration files; completes models setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development
+
+db = SQLAlchemy() # SQL Alchemy object used to interact with the db
+migrate = Migrate() # use when we need to change the structure of the db
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # DB Config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development"
+    
+    
+    db.init_app(app) # initialize SQLAlchemy object and work with the app application
+    migrate.init_app(app, db) # tell migrate to work with app, and db is how to get to the db
+
     # Register Blueprints here
     from .routes import books_bp
-
     # app.register_blueprint(hello_world_bp)
     app.register_blueprint(books_bp)
+
+    from app.models.book import Book
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,53 +1,52 @@
 
-from xml.dom import ValidationErr
 from flask import Blueprint, jsonify, abort, make_response
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-books = [
-    Book(1, "Red Fish, Blue Fish", "A Dr. Seuss book"),
-    Book(2, "The Grinch", "Another Dr. Seuss book"),
-    Book(3, "The Lorax", "The best Dr. Seuss book"),
-]
+# books = [
+#     Book(1, "Red Fish, Blue Fish", "A Dr. Seuss book"),
+#     Book(2, "The Grinch", "Another Dr. Seuss book"),
+#     Book(3, "The Lorax", "The best Dr. Seuss book"),
+# ]
 
 # hello_world_bp = Blueprint("hello_world_bp", __name__)
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except ValueError:
-        abort(make_response({"message":f"book id '{book_id}' is invalid"}, 400))
-    for book in books:
-        if book.id == book_id:
-            return book
-    abort(make_response({"message":f"book {book_id} not found"}, 404))
+# def validate_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except ValueError:
+#         abort(make_response({"message":f"book id '{book_id}' is invalid"}, 400))
+#     for book in books:
+#         if book.id == book_id:
+#             return book
+#     abort(make_response({"message":f"book {book_id} not found"}, 404))
 
-@books_bp.route("", methods=["GET"])
-def all_books():
-    books_json = []
-    for book in books:
-        books_json.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
-        )
-    return jsonify(books_json), 200 
+# @books_bp.route("", methods=["GET"])
+# def all_books():
+#     books_json = []
+#     for book in books:
+#         books_json.append(
+#             {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description,
+#             }
+#         )
+#     return jsonify(books_json), 200 
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def get_book(book_id):
-    book = validate_book(book_id)
-    return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
+# @books_bp.route("/<book_id>", methods=["GET"])
+# def get_book(book_id):
+#     book = validate_book(book_id)
+#     return {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description,
+#             }
 
 # @hello_world_bp.route("/hello-world", methods=["GET"])
 # def say_hello_world():
