bf6f9ff53d5c0402fc268ab4e0f4bc1276d393f4
Myranae <myranae@pm.me>
2022-04-28 20:44:02 -0400

    completes problem set 03 building an api

--- app/routes.py
+++ app/routes.py
@@ -6,33 +6,52 @@ from app.models.book import Book
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 @books_bp.route("", methods=["POST"])
-def handle_books():
+def create_book():
     request_body = request.get_json()
+    if "title" not in request_body or "description" not in request_body:
+        return "Invalid Request ", 400
     new_book = Book(
-        title = request_body["title"],
-        description = request_body["description"]
-    )
+        title=request_body["title"],
+        description=request_body["description"]
+        )
     db.session.add(new_book)
     db.session.commit()
+    return f"Book '{new_book.title}' created", 201
 
-    return make_response(
-        f"Book {new_book.title} created", 201
-    )
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books = Book.query.all()
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
+        )
+    return jsonify(books_response), 200 
 
+def validate_book(book_id):
+    books = Book.query.all()
+    try:
+        book_id = int(book_id)
+    except ValueError:
+        abort(make_response({"message":f"book id '{book_id}' is invalid"}, 400))
+    for book in books:
+        if book.id == book_id:
+            return book
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
 
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
+@books_bp.route("/<book_id>", methods=["GET"])
+def get_book(book_id):
+    book = validate_book(book_id)
+    return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
 
-# books = [
-#     Book(1, "Red Fish, Blue Fish", "A Dr. Seuss book"),
-#     Book(2, "The Grinch", "Another Dr. Seuss book"),
-#     Book(3, "The Lorax", "The best Dr. Seuss book"),
-# ]
-
-# hello_world_bp = Blueprint("hello_world_bp", __name__)
 
 
 # def validate_book(book_id):
