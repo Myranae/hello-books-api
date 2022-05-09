1a00944aa2cb36e582776c35a37f0f944c90431f
Myranae <myranae@pm.me>
2022-05-09 09:31:11 -0400

    adds author routes and author id foreign key

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@
 from os import abort
 from app import db
 from app.models.book import Book
+from app.models.author import Author
 from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
@@ -82,5 +83,33 @@ def delete_book(book_id):
 
     return make_response(jsonify(f"Book #{book.id} successfully deleted"))
 
+# -----> Author Blueprints
 
+@authors_bp.route("", methods=["GET"])
+def read_all_authors():
+    name_query = request.args.get("name")
+    if name_query:
+        authors = Author.query.filter_by(name=name_query)
+    else:
+        authors = Author.query.all()
 
+    authors_response = []
+    for author in authors:
+        authors_response.append(
+            {
+                "id": author.id,
+                "name": author.name,
+            }
+        )
+    return jsonify(authors_response)
+
+@authors_bp.route("", methods=["POST"])
+def create_author():
+    request_body = request.get_json()
+
+    new_author = Author(name=request_body["name"])
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return make_response(jsonify(f"Author {new_author.name} successfully created"), 201)
